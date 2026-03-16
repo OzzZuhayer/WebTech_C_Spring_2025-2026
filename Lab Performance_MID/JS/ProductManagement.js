42d58415bf38fa67ca7f6f753c185fe191992ed7
Zuhayer Hasan <zuhayerhasan103@outlook.com>
2026-03-16 15:12:41 +0600

    feat(product-management): implement total price calculation and validation This commits adds functional ability to calculate total price for 30 days, coupon on condition meet and valid user input.
    - Created 'ProductManagement.js'
    - Linked script to html
    - Added validation for quantity input field
    - Added coupon receive alert on condition meet
    - Added price calculation

---
+++ Lab Performance_MID/JS/ProductManagement.js
@@ -0,0 +1,22 @@
+const unitPrice = 1000;
+const quantityInput = document.getElementById("quantity");
+const totalPriceInput = document.getElementById("price");
+
+function calculateTotal(){
+    let quantity = parseInt(quantityInput.value) || 0;
+
+    if(quantity < 0){
+        alert("Quantity cannot be negative. Resetting to 0.");
+        quantity = 0;
+        quantityInput.value = 0;
+    }
+
+    let total = unitPrice * quantity * 30;
+    totalPriceInput.value = total;
+
+    if(total > 1000){
+        alert("Congratulations! You are eligible for a gift coupon.");
+    }
+}
+
+quantityInput.addEventListener("input", calculateTotal);
